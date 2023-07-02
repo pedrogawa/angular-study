7015669945416becd3712fb54fbb5c0bc22bc209
Pedro Ferreira <pedroh@fetch.ly>
2023-07-02 18:02:24 -0300

    Started angular study with simple todo-app

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+interface Todo {
+  id: number;
+  title: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'todoApp';
+  todoInputValue: string = '';
+  todos: Todo[] = [];
+  todosId: number = 0;
+
+  showValue() {
+    if (!this.todoInputValue) {
+      alert('Please enter a value');
+    } else {
+      this.todos.push({
+        id: this.todosId,
+        title: this.todoInputValue,
+      });
+      this.todoInputValue = '';
+      this.todosId++;
+    }
+  }
+
+  deleteTodo(id: number) {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+  }
+}
