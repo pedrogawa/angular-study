482a34d43c253696bf110d37b2d6228b785a20e4
Pedro Ferreira <pedroh@fetch.ly>
2023-07-02 23:01:42 -0300

    Just made a few changes. Need to style it better, try and use localStorage and get values from there.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,32 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 interface Todo {
   id: number;
   title: string;
+  checked: boolean;
 }
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'todoApp';
-  todoInputValue: string = '';
+  title = "todoApp";
+  todoInputValue: string = "";
   todos: Todo[] = [];
   todosId: number = 0;
 
   showValue() {
     if (!this.todoInputValue) {
-      alert('Please enter a value');
+      alert("Please enter a value");
     } else {
       this.todos.push({
         id: this.todosId,
         title: this.todoInputValue,
+        checked: false,
       });
-      this.todoInputValue = '';
+      this.todoInputValue = "";
       this.todosId++;
     }
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
